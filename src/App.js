132e38868fa94bdb36b2730e805a90dfcd232f6a
shinigami-git <nitink.kmr638@gmail.com>
2021-02-20 18:41:54 +0530

    created header COmponent file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tinder Clone</h1>
+      <Header/>
+      {/* Tinder Cards */}
+      {/* Buttons below tinder cards */}
+      {/* Chat Screen */}
+      {/* INdividual chat screen */}
     </div>
   );
-}
+}n
 
 export default App;
