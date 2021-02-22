4affe8a8cc8f87b3276f9b6413ce4ec5a46f8f72
shinigami-git <nitink.kmr638@gmail.com>
2021-02-22 15:57:50 +0530

    added icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import PersonIcon from '@material-ui/icons/Person';
-import ForumIcon from '@material-ui/icons/Forum';
-import "./header.css"
+import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+import IconButton from "@material-ui/core/IconButton";
+import "./header.css";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <PersonIcon/>
-            <h2>Header part i.e Tinder logo goes here</h2>
-            <ForumIcon/>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <IconButton className="header-icon-button" >
+        <PersonIcon className="header-icons" fontSize="large" />
+      </IconButton>
+      <img
+        className="header-logo"
+        src="https://www.thelogocreative.co.uk/wp-content/uploads/2017/09/Screen-Shot-2017-08-17-at-10.04.531.png"
+        alt="tinder-logo"
+      />
+      <IconButton className="header-icon-button">
+        <ForumIcon className="header-icons" fontSize="large" />
+      </IconButton>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
